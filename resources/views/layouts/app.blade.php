2aa8cc7df8f9b1f35f5b9def309c33583f0e5edf
Sami Nabhan <sami.nabhan20@gmail.com>
2025-11-22 12:44:55 +0200

    make edit page for engineer and adding another tabs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -476,6 +476,7 @@
         });
     </script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="{{ asset('assets/js/pace.min.js') }}"></script>
 
 </body>
 </html>
